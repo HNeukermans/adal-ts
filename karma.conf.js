fb6662339a71dc0b6a71224ed99b25a61e725bd2
Gittermans <hannes_neukermans@hotmail.com>
2016-11-27 20:29:11 +0100

    .

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function (config) {
         logLevel: config.LOG_INFO,
 
         // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: (process.env.IS_TRAVIS == null) ? true : false,
+        autoWatch: false,// (process.env.IS_TRAVIS == null) ? true : false,
 
         browsers: [
             //"Firefox",
