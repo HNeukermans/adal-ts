2d9e64baad23afc3fb804b340024e4ac578b64dd
Gittermans <hannes_neukermans@hotmail.com>
2016-12-21 23:12:09 +0100

    expôrt user  context

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@
 // export * from './adal.authentication.context';
 export * from './authentication';
 export * from './adal.config';
+export * from './user';
+export * from './authentication.context';
